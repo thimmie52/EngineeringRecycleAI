0fba5cd40a0b805f389063c684f381a88b6759e6
thimmie52 <67242050+thimmie52@users.noreply.github.com>
2025-07-15 12:21:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://eco-analyser.vercel.app/", "http://localhost:3000"], 
+    allow_origins=["https://eco-analyser.vercel.app", "http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
