eb676d30aa96558e534982a9c6489d574d5be8e7
Thimmie-oi <oluwatimilehin@amala.cloud>
2025-07-15 10:55:37 +0100

    allowed origins

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://eco-analyser.vercel.app/", "http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
